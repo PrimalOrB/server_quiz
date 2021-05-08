2ffcc281e9372ad8644802a345adcfda0d50fa78
Andrew Ogilvie <primalorb@gmail.com>
2021-05-08 14:27:47 -0400

    remove api route call for now

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const path = require( 'path' )
 const app = express();
 
 // express addons
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use( express.urlencoded( { extended: true } ) );
+app.use( express.json() );
 app.use( express.static( 'public' ) )
 
 // api routes
-const apiRoutes = require('./routes/apiRoutes');
+// const apiRoutes = require( './routes/apiRoutes' );
 // html routes
-const htmlRoutes = require('./routes/htmlRoutes');
+const htmlRoutes = require( './routes/htmlRoutes' );
 
 // app use routes
 app.use('/api', apiRoutes);
@@ -19,5 +19,5 @@ app.use('/', htmlRoutes);
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log( `Server is running on port ${PORT}.` );
 });
