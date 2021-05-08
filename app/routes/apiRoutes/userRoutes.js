44fe2e30db02b946342db7bf6170db6a507de585
Andrew Ogilvie <primalorb@gmail.com>
2021-05-08 16:14:51 -0400

    fix query for single users

--- app/routes/apiRoutes/userRoutes.js
+++ app/routes/apiRoutes/userRoutes.js
@@ -20,11 +20,11 @@ router.get( '/users', ( req, res ) => {
 
 // get single users
 router.get( '/users/:id', ( req, res ) => {
-    const sql =  `SELECT users.name, quizzes.name, questions.question, questions.answer, questions.correct
-                    FROM users
-                    JOIN quizzes ON quizzes.owner_id = users.id
-                    JOIN questions ON questions.quiz_id = quizzes.id
-                    WHERE questions.active = 1 and users.id = ?`;
+    const sql =  `SELECT users.name AS quiz_owner, quizzes.name as quiz_name, questions.question AS question, questions.answer AS quiz_answer, questions.correct AS correct_answer
+            FROM users
+            JOIN quizzes ON quizzes.owner_id = users.id
+            JOIN questions ON questions.quiz_id = quizzes.id
+            WHERE questions.active = 1 and users.id = 1;`;
     const params = [ req.params.id ]
     
     db.query( sql, params, ( err, rows ) => {
